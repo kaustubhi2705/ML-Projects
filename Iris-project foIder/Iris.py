0b2b3e73fd3c5332fb3b2f11259d4aeb6906c30f
Kaustubh Khandelwal <32143524+kaustubh2705@users.noreply.github.com>
2018-06-23 15:07:18 +0530

    Create Iris.py

---
+++ Iris-project foIder/Iris.py
@@ -0,0 +1,19 @@
+# Check the versions of libraries
+import sys as sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy as sc
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy as np
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib as plt
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas as pd
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn 
+print('sklearn: {}'.format(sklearn.__version__))
+
